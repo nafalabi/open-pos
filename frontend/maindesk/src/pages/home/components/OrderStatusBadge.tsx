b18018254254741c5f00622f9709029eecddee3e
Nanda Abi Fahmi <nafal.abi@gmail.com>
2024-06-28 23:23:20 +0700

    feat: update order status badge

--- frontend/maindesk/src/pages/home/components/OrderStatusBadge.tsx
+++ frontend/maindesk/src/pages/home/components/OrderStatusBadge.tsx
@@ -3,6 +3,7 @@ import { Badge } from "@/shared/components/ui/badge";
 
 const orderStatusText = {
   [OrderStatus.StatusPaid]: "Paid",
+  [OrderStatus.StatusExpired]: "Expired",
   [OrderStatus.StatusCanceled]: "Canceled",
   [OrderStatus.StatusPending]: "Pending",
   [OrderStatus.StatusCompleted]: "Completed",
@@ -10,6 +11,8 @@ const orderStatusText = {
 const orderStatusClassNames = {
   [OrderStatus.StatusPaid]:
     "bg-green-500 text-white hover:bg-green-500/80 hover:text-white",
+  [OrderStatus.StatusExpired]:
+    "bg-red-300 text-white hover:bg-red-300/80 hover:text-white",
   [OrderStatus.StatusCanceled]:
     "bg-red-500 text-white hover:bg-red-500/80 hover:text-white",
   [OrderStatus.StatusPending]:
@@ -19,8 +22,10 @@ const orderStatusClassNames = {
 } as const;
 
 export const OrderStatusBadge = ({ status }: { status: OrderStatus }) => {
-  const text = orderStatusText[status] ?? "Pending";
-  const className = orderStatusClassNames[status] ?? "";
+  const text = orderStatusText[status] ?? "Unknown";
+  const className =
+    orderStatusClassNames[status] ??
+    "bg-gray-600 text-gray-200 hover:bg-gray-600 hover:text-gray-200";
   return <Badge className={className}>{text}</Badge>;
 };
 
